71f1482daff980b251d78a16c1153f33246fadd4
Yolisses <iamurise@gmail.com>
2026-08-05 11:24:49 -0300

    Simplify the two fields dialog

--- src/showTwoInputDialog.tsx
+++ src/showTwoInputDialog.tsx
@@ -32,7 +32,7 @@ export async function showTwoInputDialog(): Promise<void> {
 	console.log('Available components:', Object.keys(Components));
 	console.log('LC_DESIGN_COMPONENTS_NAMES:', LC_DESIGN_COMPONENTS_NAMES);
 
-	const { Modal, Dialog, Input, Button } = Components;
+	const { Dialog, Input, Modal, Flex } = Components;
 
 	const portal = new WorkerPortal();
 	const root = new VirtualRender();
@@ -60,65 +60,36 @@ export async function showTwoInputDialog(): Promise<void> {
 
 		return (
 			<Modal
-				defaultTop={120}
-				defaultLeft={200}
-				defaultWidth={420}
-				defaultHeight={320}
-				// try common drag / close props
-				draggable={true}
-				closable={true}
-				maskClosable={true}
-				onClose={closeDialog}
-				onRequestClose={closeDialog}
-				onCancel={closeDialog}
+				height={200}
+				width={300}
+				left={100}
+				top={100}
 			>
-				<Dialog title="Enter values">
-					<div
-						style={{
-							display: 'flex',
-							flexDirection: 'column',
-							gap: 16,
-							padding: 16,
-						}}
-					>
-						{/* first field + spacing */}
-						<div style={{ marginBottom: 12 }}>
-							<div style={{ marginBottom: 6, fontSize: 13 }}>Some text</div>
-							<Input
-								placeholder="enter some text here"
-								value={text}
-								onChange={(e: any) => setText(e?.target?.value ?? e)}
-							/>
-						</div>
-
-						{/* second field + spacing */}
-						<div style={{ marginBottom: 20 }}>
-							<div style={{ marginBottom: 6, fontSize: 13 }}>Target sheet name</div>
-							<Input
-								placeholder="Sheet Name"
-								value={sheetName}
-								onChange={(e: any) => setSheetName(e?.target?.value ?? e)}
-							/>
-						</div>
-
-						{/* buttons */}
-						<div
-							style={{
-								display: 'flex',
-								justifyContent: 'flex-end',
-								gap: 8,
-								marginTop: 8,
-							}}
-						>
-							{/* try the prop names the host actually accepts */}
-							<Button label="Cancel" text="Cancel" title="Cancel" onClick={closeDialog}>
-								Cancel
-							</Button>
-							<Button label="OK" text="OK" title="OK" onClick={handleOk}>
-								OK
-							</Button>
-						</div>
-					</div>
+				<Dialog
+					title="Enter values"
+					onClose={closeDialog}
+					hide={false}
+					width={300}
+					height={200}
+					buttons={[
+						{ text: 'Cancel', onClick: closeDialog },
+						{ text: 'Ok', onClick: handleOk },
+					]}
+				>
+					<Flex direction="column" gap={10}>
+						<Input
+							type="text"
+							value={text}
+							placeholder="Google Sheet URL"
+							onChange={(e: any) => setText(e?.target?.value ?? e)}
+						/>
+						<Input
+							type="text"
+							placeholder="Sheet Name"
+							value={sheetName}
+							onChange={(e: any) => setSheetName(e?.target?.value ?? e)}
+						/>
+					</Flex>
 				</Dialog>
 			</Modal>
 		);
