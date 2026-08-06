0990004c04701d17c798f2046d43a746ce7d7183
Yolisses <iamurise@gmail.com>
2026-08-06 00:51:32 -0300

    Do a successful call to an owned endpoint

--- src/openIframe.ts
+++ src/openIframe.ts
@@ -1,4 +1,6 @@
-export function openIframe() {
+export async function openIframe() {
+	const a = await eda.sys_ClientUrl.request('https://export-easyeda-bom-to-google-sheets.vercel.app/test1', 'POST', JSON.stringify({ test1: 'nice' }));
+	console.log(a);
 	console.log('openIframe');
 	// eda.sys_IFrame.openIFrame('/iframe/google-sign-in.html', 500, 500);
 	eda.sys_IFrame.openIFrame('/iframe/test1.html', 500, 500);
